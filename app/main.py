e22e2b4d8d950021439ebc2386fafb5d5681e7d6
daghusa <humgomez410@gmail.com>
2024-02-08 07:16:15 -0500

    proyecto app

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import utils
+import read_csv
+import charts
+
+
+def run():
+  data = read_csv.read_csv('data.csv') # se elimina /app porque ya estamo en la ruta
+  data = list(filter(lambda item : item["Continent"] == "South America", data))
+
+
+  countries = list(map(lambda x: x ['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
+  country = input('type Country => ')
+
+  result =utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(labels, values)
+  
+if __name__ == '__main__': # sirve para crear dualidad, es decir que se pueda ejecutar como script o que se pueda importar 
+  run()
